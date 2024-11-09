486c06a543495d3737dfd5211dad7c8142415685
Herbert <herbetntim@gmail.com>
2024-11-09 12:37:52 +0000

    server modification for aws

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,7 +30,7 @@ app.use('/users', userRouter)
 app.use('/teams', teamRouter)
 
 // SERVER
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 5000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
