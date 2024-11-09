216833477cbf876bf1081d9fcd550aade0b01aae
Herbert <herbetntim@gmail.com>
2024-11-09 17:57:19 +0000

    post user in rds

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -13,3 +13,24 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
       .json({ message: "Error fetching users", error: error.message });
   }
 };
+
+export const postUser = async(req: Request, res: Response ) => {
+  
+  try {
+    const { username, cognitoId, profilePictureUrl = "i1.jpg", teamId = 1 } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        cognitoId,
+        profilePictureUrl,
+        teamId
+      }
+    });
+
+    res.status(201).json({message: "User created", newUser});
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Error fetching users", error: error.message });
+  }
+}
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/usersController";
+import { getUsers, postUser } from "../controllers/usersController";
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers)
+userRouter.post("/", postUser) 
+
 
 export default userRouter;
